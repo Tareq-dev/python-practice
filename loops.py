52e78823fac61e982ef601b9bcb633042630ef61
Tareq-dev <tareque.dev@gmail.com>
2025-10-14 14:19:36 +0600

    loops- while and for

---
+++ loops.py
@@ -0,0 +1,33 @@
+#WHILE LOOPS
+'''
+x = 1
+name = input("name: ")
+while x<=10:
+    print(name, x)
+    x +=1
+
+'''
+'''
+number = int(input("Your number :"))
+
+x= 1
+while x<=10:
+    print(f"{number} x {x} = {number*x}")
+    x+=1
+
+'''
+
+#FOR LOOPS
+
+# for i in range(1,12,2):
+    # print(i)
+"""
+m = int(input("enter your first number: "))
+n = int(input("enter your second number: "))
+
+ 
+for i in range(m,n-1,-1): 
+    print(m)
+    m-=1
+
+"""
