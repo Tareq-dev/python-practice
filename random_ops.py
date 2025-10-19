3734ccc30518e97491c5b92f42af17cabab6b4c3
Tareq-dev <tareque.dev@gmail.com>
2025-10-19 11:35:21 +0600

    random modules

---
+++ random_ops.py
@@ -0,0 +1,11 @@
+import random
+print(random.randint(1, 165241))
+print(random.random())
+
+names = ["Tammy", "Kanon", "Sweety","Nila","Keya"]
+print(random.choice(names))
+
+cards = ["Tammy", "Kanon", "Sweety","Nila","Keya"]
+random.shuffle(cards)
+print(cards)
+
